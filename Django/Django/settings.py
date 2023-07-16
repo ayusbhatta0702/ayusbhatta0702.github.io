3c648178e6536a8e48e401ebc5598dcfe76cf534
Ayus Bhattacharya <ayusbhatta0702@gmail.com>
2023-07-17 00:08:38 +0530

    2nd commit

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Mahiblog.apps.MahiblogConfig',
+    'Mahimail',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

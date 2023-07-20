1bd1e5dc73a132f6f35be2bdd48b27102126f6f0
Ayus Bhattacharya <ayusbhatta0702@gmail.com>
2023-07-20 14:07:43 +0530

    6th commit

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Mahiblog.apps.MahiblogConfig',
     'Mahimail.apps.MahimailConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Django/Mahiblog/views.py
+++ Django/Mahiblog/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Ayus Bhattacharya',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author': 'Ayus Bhattacharya',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2018'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts,
+        # 'posts': posts,
         #'title': 'Home'
+        'posts': Post.objects.all()
     }
     #return render(request, 'path to template file', dictionary for extra information)
     return render(request, 'Mahiblog/home.html', context)
